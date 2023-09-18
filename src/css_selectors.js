39a7210f7684204829275f93b16b1c0130476747
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2023-09-19 02:38:40 +0600

    add sponsorblock after skip duration support.

--- src/css_selectors.js
+++ src/css_selectors.js
@@ -1,2 +1,3 @@
 export const D_VIDEO_DURATION_ITEMS = 'span.ytp-time-duration';
 export const M_VIDEO_DURATION_ITEMS = 'span.time-second';
+export const SPONSORBLOCKDURATION_ELEM = 'span#sponsorBlockDurationAfterSkips';
