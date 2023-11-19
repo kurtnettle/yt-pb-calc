0e6992b49461881c7de441704ad742eb22578057
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2023-11-19 11:36:24 +0600

    fix: video element detection updated conditions of detecting video elements

--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import { getVideoElement } from './helpers.js';
 
 let hasAddedVideoListener = false;
 function addVideoListener (hasAddedVideoListener) {
-  if (hasAddedVideoListener) return;
+  if (hasAddedVideoListener) {
+    console.debug('[yt-pb-calc] [desktop] already added event listener.');
+    return true;
+  } else {
+    console.debug('[yt-pb-calc] [desktop] adding event listener.');
+  }
 
   const video = getVideoElement();
-  if (video == null) return false;
+  if (video == null) {
+    console.debug('[yt-pb-calc] [desktop] failed to get video element.');
+    return false;
+  } else {
+    console.debug('[yt-pb-calc] [desktop] got video element.');
+  }
 
   video.addEventListener('ratechange', (event) => {
     setDurationText(video.playbackRate, video.duration);
@@ -19,6 +29,12 @@ function addVideoListener (hasAddedVideoListener) {
 };
 
 document.addEventListener('yt-navigate-finish', function (event) {
+  console.debug('[yt-pb-calc] [desktop] finish navigation');
   hasAddedVideoListener = addVideoListener(hasAddedVideoListener);
   // }
+});
+
+document.addEventListener('yt-navigate-start', function (event) {
+  console.debug('[yt-pb-calc] [desktop] start navigation');
+  hasAddedVideoListener = false;
 });
