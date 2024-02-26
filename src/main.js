71529f5f1005ed83ed45efe17e12160c10c84971
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2024-02-27 03:11:43 +0600

    fix: event listeners not added upon page load.

--- src/main.js
+++ src/main.js
@@ -4,37 +4,41 @@ import { getVideoElement } from './helpers.js';
 export const DEBUG_TAG = '[yt-pb-calc] [desktop]';
 
 let hasAddedVideoListener = false;
-function addVideoListener(hasAddedVideoListener) {
+let hasAddedVideoListenerChecktimer = null;
+
+function addVideoListener () {
   if (hasAddedVideoListener) {
     console.debug(`${DEBUG_TAG} - already added event listener.`);
-    return true;
-  } else {
-    console.debug(`${DEBUG_TAG} - adding event listener.`);
+    if (hasAddedVideoListenerChecktimer !== null) {
+      console.debug(`${DEBUG_TAG} - cleared the timer.`);
+      clearInterval(hasAddedVideoListenerChecktimer);
+    }
+    return;
   }
 
   const video = getVideoElement();
   if (video == null) {
     console.debug(`${DEBUG_TAG} - failed to get video element.`);
-    return false;
+    if (hasAddedVideoListenerChecktimer === null) {
+      console.debug(`${DEBUG_TAG} - added timer.`);
+      hasAddedVideoListenerChecktimer = setInterval(addVideoListener, 150);
+    }
   } else {
-    console.debug(`${DEBUG_TAG} - got video element.`);
+    console.debug(`${DEBUG_TAG} - got video element. Adding event listener.`);
+
+    video.addEventListener('ratechange', (event) => {
+      setDurationText(video.playbackRate, video.duration);
+    });
+    video.addEventListener('playing', (event) => {
+      setDurationText(video.playbackRate, video.duration);
+    });
+
+    hasAddedVideoListener = true;
+    clearInterval(hasAddedVideoListenerChecktimer);
   }
-
-  video.addEventListener('ratechange', (event) => {
-    setDurationText(video.playbackRate, video.duration);
-  });
-  video.addEventListener('playing', (event) => {
-    setDurationText(video.playbackRate, video.duration);
-  });
-
-  return true;
 };
 
 document.addEventListener('yt-navigate-finish', function (event) {
   console.debug(`${DEBUG_TAG} - finish navigation`);
-  hasAddedVideoListener = addVideoListener(hasAddedVideoListener);
-});
-
-document.addEventListener('yt-navigate-start', function (event) {
-  console.debug(`${DEBUG_TAG} - start navigation`);
+  addVideoListener(hasAddedVideoListener);
 });
